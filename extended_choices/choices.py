eca71add17ee2e6bd9f9647896849f30f47366bf
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-06-03 17:32:36 +0800

    updated documentation & added doctests

--- extended_choices/choices.py
+++ extended_choices/choices.py
@@ -1,48 +1,72 @@
 class Choices:
     """
-    Helper to easily create objects for use with the "choices" parameter
-    for models fields.
+    Helper class for choices fields in Django.
 
-    Default call exemple, with n tuples(constant name, value, string)
+    A choice value has three representation (constant name, value and string). So
+    Choices takes list of such tuples.
+    
+    Here is an example of Choices use:
+    
+    >>> CHOICES_ALIGNEMENT = Choices(
+    ...                              ('BAD', 10, u'bad'),
+    ...                              ('NEUTRAL', 20, u'neutral'),
+    ...                              ('CHAOTIC_GOOD', 30, u'chaotic good'),
+    ...                              ('GOOD', 40, u'good'),
+    ...                             )
+    >>> CHOICES_ALIGNEMENT.BAD
+    10
+    >>> CHOICES_ALIGNEMENT.CHOICES_DICT[30]
+    u'chaotic good'
+    >>> CHOICES_ALIGNEMENT.REVERTED_CHOICES_DICT[u'good']
+    40
 
-        CHOICES_A = Choices(
-            ('THE_A_1', 11, u'a_1'),
-            ('THE_A_2', 12, u'a_2'),
-        )
+    As you can see in the above example usage, Choices objects gets three attributes:
+    - one attribute built after constant names provided in the tuple (like BAD, NEUTRAL etc...)
+    - a CHOICES_DICT that match value to string
+    - a REVERTED_CHOICES_DICT that match string to value
 
-    Default call create, for CHOICES_A :
-     * all named constant (THE_A_1 and THE_A_2)
-     * a tuple CHOICES ( (11, u'a_1'), (12, u'a_2') )
-     * a dict CHOICES_DICT { 11 => u'a_1', 12 => u'a_2' }
-     * a dict CHOICES_REVERTED { u'a_1' => 11, u'a_2' => 12 }
-
-    If you want all these names not to contain "CHOICES" but an other name,
-    you can add it to the call :
-
-        CHOICES_A = Choices(
-            ('THE_A_1', 11, u'a_1'),
-            ('THE_A_2', 12, u'a_2'),
-            name = 'FOO'
-        )
-
-    This exemple will create all constants (no changes here), and FOO will
-    be used in place of CHOICES, and FOO_DICT and FOO_REVERTED in place of
-    CHOICES_DICT and CHOICES_REVERTED
-
+    If you don't like the word CHOICES, you can provide you own nomanclature.
+    
+    >>> ALIGNEMENTS = Choices(
+    ...                       ('BAD', 10, u'bad'),
+    ...                       ('NEUTRAL', 20, u'neutral'),
+    ...                       ('CHAOTIC_GOOD', 30, u'chaotic good'),
+    ...                       ('GOOD', 40, u'good'),
+    ...                      name = 'KIND'
+    ...                      )
+    >>> ALIGNEMENTS.KIND
+    ((10, u'bad'), (20, u'neutral'), (30, u'chaotic good'), (40, u'good'))
+    >>> ALIGNEMENTS.KIND_DICT[20]
+    u'neutral'
+    >>> ALIGNEMENTS.REVERTED_KIND_DICT[u'neutral']
+    20
+                
     If you want to create other choices for the same instance, you can
     use the add_choices method
     This method take a name (to use in place of "CHOICES" in all variable
     names), and then all the tuples like in the constructor
 
-        CHOICES_A.add_choices('BAR',
-            ('THE_A_1', 11, 'a_1_bis'),
-            ('THE_A_2', 13, 'a_3'),
-        )
+    >>> CHOICES_ALIGNEMENT.add_choices('WESTERN',
+    ...                                ('UGLY', 50, 'ugly'),
+    ...                                ('CHAOTIC_UGLY', 60, 'chaotic ugly'),
+    ...                               )
+    >>> CHOICES_ALIGNEMENT.WESTERN
+    ((50, 'ugly'), (60, 'chaotic ugly'))
+    >>> CHOICES_ALIGNEMENT.WESTERN_DICT
+    {50: 'ugly', 60: 'chaotic ugly'}
+    >>> CHOICES_ALIGNEMENT.REVERTED_WESTERN_DICT
+    {'chaotic ugly': 60, 'ugly': 50}
 
-    This exemple will create all constants (except for those which already
-    exists : we don't create them and ignore the value as we get the one for
-    the existed constant), here just THE_A_2,  and add BAR, BAR_DICT and
-    REVERTED_BAR_DICT
+    Constants that already exists won't be updated according the the new value:
+
+    >>> CHOICES_ALIGNEMENT.add_choices('GOOD_KIND', 
+    ...                                 ('GOOD', 100, 'good')
+    ...                                 )
+    >>> CHOICES_ALIGNEMENT.GOOD
+    40
+
+    Last but not least you should be carful while naming your choices ``add_choices`` use is 
+    not recommended as it may trigger name clash.
     """
 
     def __init__(self, *choices, **kwargs):
@@ -63,9 +87,13 @@ class Choices:
                 value = getattr(self, const)
             CHOICES.append((value, string))
             CHOICES_DICT[value] = string
-            CHOICES_REVERTED_DICT[string] = value
+            REVERTED_CHOICES_DICT[string] = value
 
         setattr(self, name, tuple(CHOICES))
         setattr(self, '%s_DICT' % name, CHOICES_DICT)
         setattr(self, 'REVERTED_%s_DICT' % name, REVERTED_CHOICES_DICT)
 
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
